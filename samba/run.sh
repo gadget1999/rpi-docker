b72a74efc5e2d8f1fb01b6d5f6c01e8702285fd7
gadget1999 <debian08807@gmail.com>
2019-12-24 09:18:13 -0500

    use env instead args

--- samba/run.sh
+++ samba/run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+function check_env() {
+ local VARS=$1
+ for VAR in ${VARS[*]}; do
+  if [[ ${!VAR} == "" ]]; then
+   echo "Invalid ENV variables found: $VAR"
+   exit 1
+  fi
+ done
+}
+
+check_env "USER SHARE"
+
 CONFIG_FILE="/etc/samba/smb.conf"
 
 initialized=`getent passwd |grep -c '^smbuser:'`
@@ -32,91 +44,44 @@ socket options = TCP_NODELAY SO_RCVBUF=8192 SO_SNDBUF=8192
 local master = no
 dns proxy = no
 EOT
+    
+  IFS=: read username password <<<"$USER"
+  echo -n "'$username' "
+  adduser "$username" -SHD
+  echo -n "with password '$password' "
+  echo "$password" |tee - |smbpasswd -s -a "$username"
+  echo "DONE"
 
-  while getopts ":u:s:h" opt; do
-    case $opt in
-      h)
-        cat <<EOH
-Samba server container
-Container will be configured as samba sharing server and it just needs:
- * host directories to be mounted,
- * users (one or more username:password tuples) provided,
- * shares defined (name, path, users).
- -u username:password         add user account (named 'username'), which is
-                              protected by 'password'
- -s name:path:rw:user1[,user2[,userN]]
-                              add share, that is visible as 'name', exposing
-                              contents of 'path' directory for read+write (rw)
-                              or read-only (ro) access for specified logins
-                              user1, user2, .., userN
-Example:
-docker run -d -p 445:445 \\
-  -v /mnt/data:/share/data \\
-  -v /mnt/backups:/share/backups \\
-  trnape/rpi-samba \\
-  -u "alice:abc123" \\
-  -u "bob:secret" \\
-  -u "guest:guest" \\
-  -s "Backup directory:/share/backups:rw:alice,bob" \\
-  -s "Alice (private):/share/data/alice:rw:alice" \\
-  -s "Bob (private):/share/data/bob:rw:bob" \\
-  -s "Documents (readonly):/share/data/documents:ro:guest,alice,bob"
-EOH
-        exit 1
-        ;;
-      u)
-        echo -n "Add user "
-        IFS=: read username password <<<"$OPTARG"
-        echo -n "'$username' "
-        adduser "$username" -SHD
-        echo -n "with password '$password' "
-        echo "$password" |tee - |smbpasswd -s -a "$username"
-        echo "DONE"
-        ;;
-      s)
-        echo -n "Add share "
-        IFS=: read sharename sharepath readwrite users <<<"$OPTARG"
-        echo -n "'$sharename' "
-        echo "[$sharename]" >>"$CONFIG_FILE"
-        chown smbuser "$sharepath"
-        echo -n "path '$sharepath' "
-        echo "path = \"$sharepath\"" >>"$CONFIG_FILE"
-        echo -n "read"
-        if [[ "rw" = "$readwrite" ]] ; then
-          echo -n "+write "
-          echo "read only = no" >>"$CONFIG_FILE"
-          echo "writable = yes" >>"$CONFIG_FILE"
-        else
-          echo -n "-only "
-          echo "read only = yes" >>"$CONFIG_FILE"
-          echo "writable = no" >>"$CONFIG_FILE"
-        fi
-        if [[ -z "$users" ]] ; then
-          echo -n "for guests: "
-          echo "browseable = yes" >>"$CONFIG_FILE"
-          echo "guest ok = yes" >>"$CONFIG_FILE"
-          echo "public = yes" >>"$CONFIG_FILE"
-        else
-          echo -n "for users: "
-          users=$(echo "$users" |tr "," " ")
-          echo -n "$users "
-          echo "valid users = $users" >>"$CONFIG_FILE"
-          echo "write list = $users" >>"$CONFIG_FILE"
-          echo "admin users = $users" >>"$CONFIG_FILE"
-        fi
-        echo "DONE"
-        ;;
-      \?)
-        echo "Invalid option: -$OPTARG"
-        exit 1
-        ;;
-      :)
-        echo "Option -$OPTARG requires an argument."
-        exit 1
-        ;;
-    esac
-  done
-
+  IFS=: read sharename sharepath readwrite users <<<"$SHARE"
+  echo -n "'$sharename' "
+  echo "[$sharename]" >>"$CONFIG_FILE"
+  chown smbuser "$sharepath"
+  echo -n "path '$sharepath' "
+  echo "path = \"$sharepath\"" >>"$CONFIG_FILE"
+  echo -n "read"
+  if [[ "rw" = "$readwrite" ]] ; then
+    echo -n "+write "
+    echo "read only = no" >>"$CONFIG_FILE"
+    echo "writable = yes" >>"$CONFIG_FILE"
+  else
+    echo -n "-only "
+    echo "read only = yes" >>"$CONFIG_FILE"
+    echo "writable = no" >>"$CONFIG_FILE"
+  fi
+  if [[ -z "$users" ]] ; then
+    echo -n "for guests: "
+    echo "browseable = yes" >>"$CONFIG_FILE"
+    echo "guest ok = yes" >>"$CONFIG_FILE"
+    echo "public = yes" >>"$CONFIG_FILE"
+  else
+    echo -n "for users: "
+    users=$(echo "$users" |tr "," " ")
+    echo -n "$users "
+    echo "valid users = $users" >>"$CONFIG_FILE"
+    echo "write list = $users" >>"$CONFIG_FILE"
+    echo "admin users = $users" >>"$CONFIG_FILE"
+  fi
+  echo "DONE"
 fi
 
-exec ionice -c 3 smbd -FS --configfile="$CONFIG_FILE" < /dev/null
+exec smbd -FS --configfile="$CONFIG_FILE" < /dev/null
